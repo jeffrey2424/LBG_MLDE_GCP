44cb07a5a0e8a4bf1fe077b6e6cd5303c986d6ed
Jeffrey Sharpe <jeffrey.t.sharpe@gmail.com>
2021-09-07 18:43:29 +0100

    Attempts to GroupByKey

--- apache_beam_demo.py
+++ apache_beam_demo.py
@@ -3,8 +3,9 @@
 from apache_beam.options.pipeline_options import PipelineOptions
 import apache_beam as beam
 
+
 input_file = './input_data.json'
-output_file = './output_data.json'
+output_file = './output_data'
 
 pipelineOptions = PipelineOptions(
     runner='DirectRunner',
@@ -12,8 +13,16 @@ pipelineOptions = PipelineOptions(
     job_name='Demo_Job',
 )
 
+print("Test print")
+
 with beam.Pipeline(options=pipelineOptions) as demoPipeline:
     input_data = ( demoPipeline
                   | "readFromFile" >> beam.io.ReadFromText(input_file)
-                  | "writeToFile" >> beam.io.WriteToText(output_file)
+                  # | "Map" >> beam.Map(lambda element: (element[0], element[1]))
+                  # | "GroupByKey" >> beam.GroupByKey()
+                  # | "mapTuple" >> beam.MapTuple(lambda item: '{}{}'.format(item[0], item[1]))
+                  | "writeToFile" >> beam.io.WriteToText(output_file,
+                                                         file_name_suffix=".json",
+                                                         shard_name_template=''
+                                                         )
                   )
